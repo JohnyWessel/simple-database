e062ed92860483cf03a24549026079c00d656c53
JohnyWessel <145611908+JohnyWessel@users.noreply.github.com>
2024-09-02 13:59:34 +0300

    Update wessql.py

--- wessql.py
+++ wessql.py
@@ -1,8 +1,6 @@
 import sqlite3
 
 global direct
-
-
 def setdirecritry(directory: str):
    global direct
    direct = directory
@@ -38,6 +36,28 @@ class table():
          if i[0] == target[0]:
             cursor.execute(f"DELETE FROM {table} WHERE {collumn} = ?", target)
             connect.commit()
+
+   def Edit(table_name: str, collumn: str, target: list, item):
+      connect = sqlite3.connect(direct)
+      cursor = connect.cursor()
+      cursor.execute(f"SELECT * FROM {table_name}")
+      x = cursor.fetchall()
+      buff,items,count = [],[],0
+      for i in x:
+         for j in range(0,len(i)):
+            if i[j] == target:
+               buff+=i
+               count=j
+               cursor.execute(f"DELETE FROM {table_name} WHERE {collumn} = ?", [target])
+               connect.commit()
+               break
+      for k in range(len(buff)):
+         if k != count:
+            items.append(buff[k])
+         else:
+            items.append(item)
+      table.AddToTable(table_name, items)
+      connect.commit()
 
 class search():
    def item(table: str, target: list) -> list:
